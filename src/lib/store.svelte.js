b7114b4b9812f0dba35bda00ffb6f255216259ea
kalt08 <kaltenegger.laura@johak.at>
2025-12-17 13:07:46 +0100

    Update store.svelte.js

--- src/lib/store.svelte.js
+++ src/lib/store.svelte.js
@@ -31,8 +31,9 @@ export let store = $state({
 		await pb.collection('rabbits').delete(id);
 		store.listRabbits();
 	},
-	addRabbit: async (name, rabbithole) => {
-		const response = await pb.collection('rabbits').create({ name, rabbithole: rabbithole });
+	addRabbit: async (rabbit) => {
+		const response = await pb.collection('rabbits').create(rabbit);
+		store.listRabbits();
 		console.log(response);
 	}
 });
