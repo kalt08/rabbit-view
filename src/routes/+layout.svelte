a42258bda159604d3b22bf12b2edbd2ba0a7be67
kalt08 <kaltenegger.laura@johak.at>
2026-03-11 12:16:17 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
 	import '../app.css';
-	import { store } from '$lib/store.svelte';
+	import { pb, store } from '$lib/store.svelte';
 	import favicon from '$lib/assets/favicon.svg';
+	import { goto } from '$app/navigation';
 
 	let { children } = $props();
 	let theme = $state('halloween');
@@ -11,6 +12,9 @@
 	}
 	// unser "Konstruktor" (lifecycle hook) - läuft jedesmal, wenn die Seite bzw. die Komponente geladen wird:
 	$effect(() => {
+		if (!pb.authStore.isValid) {
+			goto('/auth');
+		}
 		store.listRabbits();
 		if (localStorage.getItem('theme')) theme = localStorage.getItem('theme');
 		$inspect('🐰: ', store.rabbits);
